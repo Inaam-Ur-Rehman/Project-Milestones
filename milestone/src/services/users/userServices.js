6bc093de7f3fbaead5467fd354e1a323e5c9ac5a
Shubham Kumar <johndoe@email.com>
2024-08-19 14:07:29 +0530

    Third Commit

--- milestone/src/services/users/userServices.js
+++ milestone/src/services/users/userServices.js
@@ -3,10 +3,14 @@ import { BASE_URL } from "../../utils/url";
 
 //! Login User
 export const loginAPI = async ({ email, password }) => {
-  const response = await axios.post(`${BASE_URL}/login`, {
-    email,
-    password,
-  });
+  const response = await axios.post(
+    `${BASE_URL}/login`,
+    {
+      email,
+      password,
+    },
+    { withCredentials: true }
+  );
   //! Return a promise
   return response.data;
 };
--- server/index.js
+++ server/index.js
@@ -7,7 +7,11 @@ import cors from "cors";
 
 // Create the server
 
-const server = http.createServer(app);
+const server = http.createServer(app, (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+});
 const PORT = process.env.PORT || 3001;
 
 // const app = express();
