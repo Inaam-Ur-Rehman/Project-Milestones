e1d58383cd17cc4649138db2c861f8b2209e8f3b
Inaam Ur Rehman <inaamurrehman343@gmail.com>
2024-08-25 08:30:48 +0500

    Added: CSV upload functionality to backend

--- server/index.js
+++ server/index.js
@@ -3,7 +3,13 @@ import mongoose from "mongoose";
 import cors from "cors";
 import employeeRoute from "./Router/employeeRoute.js";
 import dotenv from "dotenv";
+import multer from "multer";
+import csv from "csvtojson";
+import Product from "./models/Product.js";
+
 dotenv.config();
+
+const upload = multer();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -26,10 +32,33 @@ app.get("/", (req, res) => {
   res.send("CORS is working!");
 });
 
+app.post("/upload-csv", upload.single("file"), (req, res) => {
+  const file = req.file;
+
+  csv()
+    .fromString(file.buffer.toString())
+    .then((json) => {
+      // skip headers and Serial number column
+      json.shift();
+      json.forEach((element) => {
+        delete element["S.N."];
+      });
+
+      Product.insertMany(json)
+        .then(() => {
+          return res.status(200).json("File uploaded successfully");
+        })
+        .catch((err) => {
+          return res.status(400).json(err);
+        });
+    });
+});
+
 // Connect to MongoDB
 mongoose
   .connect(
-    "mongodb+srv://mydata:Lh27AfZvJb0yaryv@milestone.fnfk0px.mongodb.net/employee"
+    // "mongodb+srv://mydata:Lh27AfZvJb0yaryv@milestone.fnfk0px.mongodb.net/employee"
+    "mongodb://localhost:27017/employee"
   )
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
